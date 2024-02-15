2a0b66c0d27c2e90dcd1bceeab786b904e32cf53
Finn <fmartsula@gmail.com>
2024-02-15 10:04:05 -0700

    Initialization of project

---
+++ AlgorithmSelection.R
@@ -0,0 +1,18 @@
+# Finn Tomasula Martin
+# COSC-4557
+# ML Algorithm Selection
+# This file contains code testing the performance of various machine learning algorithms run on the same dataset.
+
+# Clear environment
+rm(list = ls())
+dev.off()
+
+# Load in data
+wine <- read.csv("winequality-red.csv", sep=";")
+
+
+
+
+
+
+
